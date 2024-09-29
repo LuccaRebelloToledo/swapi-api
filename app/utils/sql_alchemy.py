1048bfe61803f1460280907d4f0243e576645f84
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-09-29 18:35:40 -0300

    refactor: remove unnecessary code in save_record

--- app/utils/sql_alchemy.py
+++ app/utils/sql_alchemy.py
@@ -17,20 +17,6 @@ def save_record(model, data: dict, record_id: str, keys_to_process: list):
     db.session.add(new_record)
     db.session.commit()
     return new_record
-    existing_record = model.query.get(record_id)
-    
-    if existing_record:
-        for key, value in data.items():
-            setattr(existing_record, key, value)
-        db.session.commit()
-        return existing_record
-    
-    data['id'] = record_id
-
-    new_record = model(**data)
-    db.session.add(new_record)
-    db.session.commit()
-    return new_record
 
 def update_record(existing_record, data, keys_to_process):
     process_kwargs(existing_record, data, keys_to_process)
